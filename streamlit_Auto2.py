4475b2e8367a722728629d9068a9f1bec3725a3f
Nayeongseo0217 <nays43883@naver.com>
2024-08-04 13:07:19 +0900

    Update streamlit_Auto2.py

--- streamlit_Auto2.py
+++ streamlit_Auto2.py
@@ -2123,13 +2123,6 @@ if choice == "온라인 상담사와 함께하는 모빌리티 컨설팅":
             thread_id=thread_id,
         )
     
-        # ionicons CSS 및 JS 파일 포함
-        st.markdown('''
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/5.5.2/collection/components/icon/icon.min.css">
-            <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-            <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-        ''', unsafe_allow_html=True)
-    
         # 어시스턴트의 응답 메시지 출력
         for msg in reversed(messages.data):
             role = "나" if msg.role == "user" else "오토커넥트 챗봇"
@@ -2156,7 +2149,9 @@ if choice == "온라인 상담사와 함께하는 모빌리티 컨설팅":
                         <div style="text-align: left; margin-bottom: 10px;">
                             <div style="display: flex; align-items: flex-start;">
                                 <div style="flex-shrink: 0; border-radius: 50%; width: 40px; height: 40px; background-color: #A0B4F2; display: flex; align-items: center; justify-content: center; margin-right: 10px;">
-                                    <ion-icon name="chatbubble-ellipses-outline" style="width: 24px; height: 24px; color: white;"></ion-icon>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" class="bi bi-car-front-fill" viewBox="0 0 16 16">
+                                        <path d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679q.05.242.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.8.8 0 0 0 .381-.404l.792-1.848ZM3 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2m10 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2M6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2zM2.906 5.189a.51.51 0 0 0 .497.731c.91-.073 3.35-.17 4.597-.17s3.688.097 4.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 11.691 3H4.309a.5.5 0 0 0-.447.276L2.906 5.19Z"/>
+                                    </svg>
                                 </div>
                                 <div style="display: flex; flex-direction: column; flex-grow: 1;">
                                     <strong>{role}</strong>
